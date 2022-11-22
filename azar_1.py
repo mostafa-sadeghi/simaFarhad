cf3fb53779a1a5419ae6895f7eda952636c20369
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-22 22:58:38 +0330

    1azar

--- azar_1.py
+++ azar_1.py
@@ -1,1 +1,46 @@
-print("hello world")
+# print("hello world")
+
+# primitive types : numbers , str , bool
+
+# name = "ali"
+# name = 'ali'
+# x = 1
+
+# print("name is : ", name)
+# print("x is : ", x)
+
+# print(type(x))
+# print(type(name))
+
+# s = ''
+# print(type(s))
+
+
+# z = 1.0
+
+# print(type(z))
+
+
+# print("""afdhafhksfhka
+# fkafjkhdfahjsfgahsj""")
+# print('''afdhafhksfhka
+# fkafjkhdfahjsfgahsj''')
+
+# message = 'he said "some thing"'
+# print(message)
+# message = "he said \"some thing\""
+# print(message)
+
+# message = 'should\'nt'
+# message = """should'nt"""
+
+
+score = 1000
+message = "i scored %s" % score
+print(message)
+
+
+name = "tina"
+age = 28
+message = "%s is %s years old" % (name, age)
+print(message)

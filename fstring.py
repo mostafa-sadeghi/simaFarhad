8a3f83ee86c3a056cd258ab08f994cf06539b962
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-29 23:01:04 +0330

    adadasd

---
+++ fstring.py
@@ -0,0 +1,64 @@
+# name = "ali"
+# family = "rezaei"
+
+# message = f'hello {name} {family}'
+# print(message)
+
+#x = input('enter a number ')
+#print('x is',x)
+#print(type(x))
+
+#y = int(x) + 1
+#print(y)
+
+name = 'ali'
+family = 'rezaei'
+
+student = name + ' ' + family
+print(student)
+
+
+print('*' * 3)
+
+name = 'tina'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+print(name[1:3])
+print(name[-1])
+
+print(len(name))
+
+#name[0] = 's' > error
+
+# list
+
+shopping_list = 'item1' + 'item2'
+print(shopping_list[0])
+
+shopping_list = []
+print(type(shopping_list))
+
+shopping_list.append('item1')
+shopping_list.append('item2')
+print(shopping_list)
+print(shopping_list[0])
+shopping_list[0] = 'item3'
+print(shopping_list)
+
+#niklearning.ir
+
+
+
+
+
+
+
+
+
+
+
+
+
